7ca06b490ec9d4adc57656d61b1a06996b7ecb18
Sayan Paul <sayanpaul2016@gmail.com>
2023-12-30 22:06:17 +0530

    update date

--- Main.java
+++ Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
-        System.out.println("Time : " + new Date());
+        System.out.println("Date & Time : " + new Date());
     }
 }
